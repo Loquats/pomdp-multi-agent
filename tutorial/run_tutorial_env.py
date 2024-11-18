2898a1a5e632012f4fa1a4f94462dd112ae57338
Andy Zhang <yueandyzhang@gmail.com>
2024-11-17 18:56:48 -0800

    policy templates

--- tutorial/run_tutorial_env.py
+++ tutorial/run_tutorial_env.py
@@ -8,6 +8,17 @@ print(env.action_space(env.agent_names[0]))
 print(env.action_space(env.agent_names[1]))
 print(observations)
 
+def template_policy(observation, action_space, agent):
+    pass
+
+def heuristic_policy(observation, action_space, agent):
+    """
+    find which quadrant the opponent is in
+    move in one of the directions towards the opponent (break ties randomly)
+    gaze in the direction of the opponent (break ties randomly)
+    """
+    pass
+
 def random_policy(action_space):
     return action_space.sample()
 
